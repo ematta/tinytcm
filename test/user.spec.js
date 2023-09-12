11fb144f94a06417b20aa679232bfd80760d1f6d
Enrique Matta-Rodriguez <enrique.j.matta@gmail.com>
2023-09-11 20:32:11 -0500

    test: initial set

---
+++ test/user.spec.js
@@ -0,0 +1,21 @@
+import { describe, expect, it } from "bun:test";
+import { app } from "../src/index";
+
+describe("Elysia", () => {
+  it("return a response", async () => {
+    const response = await app
+      .handle(
+        new Request("http://localhost/user/sign-up", {
+          method: "POST",
+          body: { 
+            name: "Enrique Matta", 
+            email: "enrique@matta.dev",
+            password: "test" 
+          },
+        })
+      )
+      .then((res) => res.json());
+
+      expect(response.message).toBe("Sign up successfully");
+  });
+});
