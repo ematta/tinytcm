66c0888cf01ab9da0c7d9e1dba3f8a60d62303de
Enrique Matta-Rodriguez <enrique.j.matta@gmail.com>
2023-09-26 17:00:51 -0500

    chore: moved models

---
+++ src/models/organization.js
@@ -0,0 +1,13 @@
+import { DataTypes } from "sequelize";
+
+export const Organization = {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+};
---
+++ src/models/user.js
@@ -0,0 +1,26 @@
+import { DataTypes } from "sequelize";
+
+export const User = {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+};
